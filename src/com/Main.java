d286925938405bc3937bf57dd93a79f13f49a802
LeandroLopesM <leandrolopes.tinfem2023@gmail.com>
2024-08-09 23:34:19 -0300

    Moved stack test code to separate class file

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,52 +1,9 @@
 package com;
 
-import com.stack.Stack;
 
-public class Main {
-	private Main() {
-//		String polistNote = "5 6 + 9 - 10 +"; // ((5 + 6) - 9) + 10 = 12;
-//		String polishNote = "9 3 * 2 + 10 - 1 - 2 /"; // ((((9 * 3) + 2) - 10) - 1) /2
-		String polishNote = "5 10 * 2 / 13 - 8 *"; // (((5 * 10) / 2) - 13) * 8 = 96;
-		
-		Stack s = new Stack( polishNote.length() );
-				
-		for( String c : polishNote.split(" ") ) {
-			if( isInt(c) ) {
-				s.push(c);
-			}
-			else if( isOperator( c ) ) {
-				int rhs = Integer.parseInt("" + s.pop());
-				int lhs = Integer.parseInt("" + s.pop());				
-				
-				s.push( operate( lhs, rhs, c ) );
-			}
-		}
-		System.out.println("GetPolishedNerd => " + s.pop());
-	}
-	
-	public Integer operate( Integer lhs, Integer rhs, String op ) {
-		switch( op ) {
-			case "/": return lhs / rhs;
-			case "*": return lhs * rhs;
-			case "-": return lhs - rhs;
-			case "+": return lhs + rhs;
-			default: return 0;
-		}
-	}
-	
-	private boolean isInt(String s) {
-		try {
-			Integer.parseInt(s);
-		} catch( NumberFormatException e) {
-			return false;
-		}
-		
-		return true;
-	}
-	
-	public boolean isOperator( String o ) { return o.matches("(\\+|-|\\*|\\/)"); }
-	
+
+public class Main {	
 	public static void main( String...args ) {
-		new Main();
+		new TestStack(null);
 	}
 }
